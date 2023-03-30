29d3f4cf572decd9e19fb40d727d787bc66894aa
LingGrani <luthfiangger2510@gmail.com>
2023-03-30 16:09:46 +0700

    Debug 1

--- Quicksort_137/Quicksort_137.cpp
+++ Quicksort_137/Quicksort_137.cpp
@@ -38,9 +38,7 @@ void q_sort(int low, int high)
 {
     int pivot, i, j;
     if (low > high)             //Langkah Algoritma no. 1
-    {
         return;
-    }
     //partition the list into two parts
     //one containing elements less that or equal to pivot
     //Outher containning elements greater than pivot
@@ -105,14 +103,3 @@ int main()
     system("pause");
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

fc0410d34b2a67586711cfd32296d68d67c70e98
LingGrani <luthfiangger2510@gmail.com>
2023-03-30 16:18:18 +0700

    Debug 2

--- Quicksort_137/Quicksort_137.cpp
+++ Quicksort_137/Quicksort_137.cpp
@@ -57,7 +57,7 @@ void q_sort(int low, int high)
         }
         cmp_count++;
         //search for an element less than or equal to pivot
-        while ((arr[i] >= pivot) && (j >= low)) //Langkah Algoritma no. 7
+        while ((arr[j] >= pivot) && (j >= low)) //Langkah Algoritma no. 7
         {
             j--;                    //Langkah Algoritma no. 8
             cmp_count++;
